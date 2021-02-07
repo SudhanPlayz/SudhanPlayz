fb2e554a553160a676e9e6ec039eda7f92598f30
Sudhan <sudhanplayz@gmail.com>
2021-02-07 08:29:10 +0530

    ;-;

--- src/update.js
+++ src/update.js
@@ -66,7 +66,7 @@ Check [How I became a developer by clicking here :)](https://github.com/SudhanPl
     <img align="left" src="https://github-readme-stats.vercel.app/api?username=SudhanPlayz&theme=tokyonight"><img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=SudhanPlayz&theme=tokyonight&hide=batchfile">
 </details>
 <!-- Last updated on ${date.toString()} ;-;-->
-*Last updated on ${date.getDate()}${date.getDate()===1?"st":date.getDate()===2?"nd":date.getDate()===3?"rd":"th"} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]} ${date.getFullYear()} using magic✨*`
+*Last updated on ${date.getDate()}${date.getDate()===1?"st":date.getDate()===2?"nd":date.getDate()===3?"rd":"th"} ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][date.getMonth()]} ${date.getFullYear()} using magic* ✨`
 
     //Saving on readme.md
     fs.writeFileSync(ReadMe, text)
