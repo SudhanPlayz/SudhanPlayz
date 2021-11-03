aaa7d10783bcfaa97d3ccdb88abce95cf27a904f
Sudhan <sudhanplayz@gmail.com>
2021-11-03 18:07:55 +0530

    Update update.js

--- src/update.js
+++ src/update.js
@@ -27,7 +27,7 @@ const WriteReadMe = async () => {
 
   //Creating the text what we gonna save on ReadMe file
   const text = `## Hi there 👋 <img align="right" src="https://avatars.githubusercontent.com/u/55418697?v=4" width="200" />
-I'm **Sudhan**, An developer from India. I like to code web applications and games. I have worked on many projects in my past, Some of my open source projects are pinned below make sure to check them out.
+I'm **Sudhan**, An developer from somewhere in the earth. I like to code web applications and games. I have worked on many projects in my past, Some of my open source projects are pinned below make sure to check them out.
   
 Thanks for visiting my github profile. Have a great day ahead!~
   
